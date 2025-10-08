0c65f5cf27fbdcd86ec36ada36e067b6c743f0d2
Li-KangNing <3543892593@qq.com>
2025-10-08 20:59:40 +0800

    员工分页查询业务代码开发

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -41,7 +41,7 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
             Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
-            log.info("当前员工id：", empId);
+            log.info("当前员工id：{}", empId);
             //存储当前登录的用户id到ThreadLocal中
             BaseContext.setCurrentId(empId);
             //3、通过，放行
